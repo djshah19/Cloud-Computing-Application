312d4abce968a9e8a771888046bba1227127d7e8
IvanFernando <43161650+ivanrvfernando@users.noreply.github.com>
2018-11-05 22:25:10 -0500

    Lambda Function without credential

--- LambdaApp/src/main/java/Email.java
+++ LambdaApp/src/main/java/Email.java
@@ -1,6 +1,7 @@
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
@@ -18,16 +19,16 @@ public class Email implements RequestHandler<Object,String> {
     public String handleRequest(Object o, Context context) {
         context.getLogger().log("Testing CloudWatch logging");
         try {
-            AmazonDynamoDB clientDynamoDB = AmazonDynamoDBClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("AKIAJ57D5A4O35BGNYDA","QDFmXU5EQ5FUZAr17YvGcs5edbT7MhyK4WFXtwMW"))).build();
+            AmazonDynamoDB clientDynamoDB = AmazonDynamoDBClientBuilder.standard().withRegion("us-east-1").build();
+            //clientDynamoDB.setEndpoint("arn:aws:dynamodb:us-east-1:147432315547:table/password_reset");
             DynamoDB dynamoDB = new DynamoDB(clientDynamoDB);
 
             Table table = dynamoDB.getTable("password_reset");
 
             Item item = table.getItem("id", "346575-565656");
-
+            context.getLogger().log(item.getString("username"));
             AmazonSimpleEmailService client =
-                    AmazonSimpleEmailServiceClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("AKIAIORQRBGBBU5CI2QA", "Db/I7J5s8vNaeO6i4s6mhx2gylPvuyDq9OIXObUu"))).build();
-
+                    AmazonSimpleEmailServiceClientBuilder.standard().withRegion("us-east-1").build();
             SendEmailRequest request = new SendEmailRequest()
                     .withDestination(
                             new Destination().withToAddresses(item.getString("username")))
