c01d3c27b900aedca53dd44eacfcfce0b7ac2a33
IvanFernando <43161650+ivanrvfernando@users.noreply.github.com>
2018-11-13 18:48:43 -0500

    moved lambda execution in serverlessstack

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-serverless-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-serverless-stack.sh
@@ -6,21 +6,12 @@ stack_name=$1
 
 myS3Bucket=$2
 
-myLambdaIAMRole=$3
-
-mySNSName=$4
 #CICDstack_name=$4
 
 bucketname="${myS3Bucket}"
 
-lambdaIAMRole="${myLambdaIAMRole}"
-snsname="${mySNSName}"
 
-lambdaIAMRoleArn=$(aws iam get-role --role-name lambdaIAMRole --query Role.Arn --output text)
-
-
-
-aws cloudformation create-stack --stack-name $stack_name --template-body file://csye6225-cf-serverless.json --parameters ParameterKey=myBucketName,ParameterValue=$bucketname ParameterKey=lambdaIAMRole,ParameterValue=$lambdaIAMRoleArn ParameterKey=SNSNAME,ParameterValue=$snsname --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --stack-name $stack_name --template-body file://csye6225-cf-serverless.json --parameters ParameterKey=myBucketName,ParameterValue=$bucketname --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
 #ParameterKey=ListS3BucketsRole,ParameterValue=$EC2_role
 
 
