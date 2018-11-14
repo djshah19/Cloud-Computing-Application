fe60dc4d3a37e7a7ea4718499c899bdd383bfbdb
djshah19 <38405786+djshah19@users.noreply.github.com>
2018-11-13 22:36:57 -0500

    updated afterinstall.sh

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -3,7 +3,7 @@
 sudo systemctl stop tomcat.service
 
 echo "Updating Lambda Function"
-  aws lambda update-function-code --function-name myLambda --region us-east-1 --s3-bucket lambda.fernandoi.me.csye6225.com --s3-key LambdaApp-2.2.jar
+  aws lambda update-function-code --function-name myLambda --region us-east-1 --s3-bucket lambda.$domain.csye6225.com --s3-key LambdaApp-2.2.jar
 echo "Update complete"
 
 sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
